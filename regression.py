8a7004f8b9aff7e3bcab9e38071f82b2c309ad12
andrewng1229 <144169286+andrewng1229@users.noreply.github.com>
2024-01-25 18:03:44 -0500

    Update regression.py

--- regression.py
+++ regression.py
@@ -30,8 +30,8 @@ def fit_regression_model(X, y):
     Hint 2: while woring you can use the print function to print the loss every 1000 epochs.
     Hint 3: you can use the previos_loss variable to stop the training when the loss is not changing much.
     """
-    learning_rate = 0.01 # Pick a better learning rate
-    num_epochs = 4000# Pick a better number of epochs
+    learning_rate = 0.0002 # Pick a better learning rate
+    num_epochs = 5000# Pick a better number of epochs
     input_features = X.size(1) # extract the number of features from the input `shape` of X
     output_features = y.size(1) # extract the number of features from the output `shape` of y
     model = create_linear_regression_model(input_features, output_features)
